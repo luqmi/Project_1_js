5dd84fa3291b774f0b69049d3e65197747c7178d
khan bahe <115805301+luqmi@users.noreply.github.com>
2024-02-22 03:59:17 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+document.write('<center><h1>( CV )</h1></center>');
+
+document.write('</br><center><h2>NAME: &nbsp Luqman khan</h2></center>')
+document.write('<center><h2>Contact Number: &nbsp (123) 456-7890 </h2></center>')
+document.write('<center><h2>Contact Number: &nbsp Luqmankhansuffah789@gmail.com </h2></center>')
+document.write('<center><h2>Adress: &nbsp Baldia Town Karachi</h2></center>')
+
+
+
+document.write('</br><span><h1>Profile</h1></span>')
+document.write('<center><h4>Business Administration student.I consider my self a responsible and oderly person.I am looking forward for my first work experience.</h4></center>')
+
+
+
+
+
+
+
+
+document.write('</br><span><h1>Education</h1></span>')
+
+document.write('<table><tr><th><h4>Indus University</h4></th><td><h4>Software Engineering career,in progress</h4></td></tr><tr><th><h4>SM Science College</h4></th><td><h4>Pre Engineering</h4></td></tr></table>')
+
+
+
+document.write('</br><span><h1>Language</h1></span>')
+document.write('<table><tr><th><h4>English</h4></th><th><h4>Urdu</h4></th><th><h4>Pashto</h4></th></tr></table>')
+
+
+
+// document.write('</br><center><h1>Skills</h1></center>')
+document.write('</br><span><h1>Skills</h1></span>')
+document.write('<span><h4>MS Office</h4></span>')
+document.write(' <span><h4>Digital Marketing</h4></span>')
+document.write('<span><h4>Web Developer</h4></span>')
+document.write('<span><h4>Hafiz-e-Quran</h4></span>')
+
+
+
+
+document.write('</br><span><h1>Experience</h1></span>')
+document.write('<span><h4>Digital Marketing: &nbsp 2 Years</h4></span>')
+document.write('<span><h4>Hafiz-e-Quran: &nbsp 6 Years</h4></span>')
+
+
+document.write('</br><span><h1>Reference</h1></span>')
+document.write('<span><h4>Availiable Upon Request</h4></span>')
